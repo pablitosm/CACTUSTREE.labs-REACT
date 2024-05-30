0467d2dd7a0e4061090bdc9d7e04d09d5905eb81
Pablo San Miguel <99831806+pablitosm@users.noreply.github.com>
2024-05-30 13:58:08 +0200

    art

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -71,14 +71,7 @@ function Art() {
             <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`} style={image.style}></div>
           ))}
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+       
       </div>
 
 
