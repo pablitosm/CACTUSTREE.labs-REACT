4a59772af480ef5d9cf8ff4a55e885513b224e34
Pablo San Miguel <99831806+pablitosm@users.noreply.github.com>
2026-01-05 22:30:58 +0100

    cambios 2026

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,11 +32,11 @@ function Contact() {
       <div className="bottom-content-contact" style={{ padding: '0 15px 15px 15px', width: '100%' }}>
         <div className="centered-text2">
           <h2 className="aboutMe">ABOUT ME</h2>
-          <p>MY NAME IS <b>ARTURO MARTÍN</b>, I AM A <b>21-YEAR-OLD</b> GUY WHO IS PASSIONATE ABOUT THE <b>AUDIOVISUAL WORLD</b>. SINCE I WAS A CHILD, I&apos;VE ALWAYS HAD A CAMERA IN MY HANDS. I GRADUATED FROM THE <b>MOST IMPORTANT AUDIOVISUAL PRODUCTION SCHOOL IN ARAGON</b>. AFTER GRADUATING, I WENT TO <b>FRANCE TO WORK</b> FOR A WHILE AT A PRODUCTION COMPANY.</p>
+          <p>MY NAME IS <b>ARTURO MARTÍN</b>, I AM A <b>23-YEAR-OLD</b> GUY WHO IS PASSIONATE ABOUT THE <b>AUDIOVISUAL WORLD</b>. SINCE I WAS A CHILD, I&apos;VE ALWAYS HAD A CAMERA IN MY HANDS. I GRADUATED FROM THE <b>MOST IMPORTANT AUDIOVISUAL PRODUCTION SCHOOL IN ARAGON</b>. AFTER GRADUATING, I WENT TO <b>FRANCE TO WORK</b> FOR A WHILE AT A PRODUCTION COMPANY.</p>
           <p>NOW THAT I&apos;M BACK HOME, I&apos;M LOOKING FOR A WAY TO <b>START MY OWN BUSINESS</b>, AND I AM ACTIVELY SEEKING <b>NEW CLIENTS</b> WHO WANT TO <b>ENHANCE THEIR BUSINESSES</b> THROUGH IMAGES AND VIDEOS.</p>
           <p>THAT&apos;S WHY I&apos;VE CREATED <b>CACTUSTREE.LABS&#8482;</b>. FOR ME, AUDIOVISUAL PRODUCTION IS ANOTHER WAY TO EXPRESS MYSELF, AND I STRIVE TO ENSURE THAT ALL MY CLIENTS ARE SATISFIED WITH THE WORK I DO.</p><br />
           <p>PHONE NUMBER: <b><a className="link-footer-index-description" href="tel:+34692948126">+34 692 94 81 26</a></b></p>
-          <p style={{ margin: 0 }}>EMAIL: <b><a className="link-footer-index-description" href="mailto:info@cactustreelabs.com">INFO@CACTUSTREELABS.COM</a></b></p>
+          <p style={{ margin: 0 }}>EMAIL: <b><a className="link-footer-index-description" href="mailto:info@cactustreelabs.es">INFO@CACTUSTREELABS.ES</a></b></p>
         </div>
       </div>
       <div className="top-content-contact rightContent" style={{ padding: '0 15px 0 15px', width: '100%' }}>
